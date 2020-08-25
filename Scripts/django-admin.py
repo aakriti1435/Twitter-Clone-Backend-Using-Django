c3b539d1324e73c49a550876a7073b1acd803cb4
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2020-08-25 14:23:22 +0530

    Initial Commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aakri\OneDrive\Desktop\Twitter Clone\Dark Theme Twitter Clone Using Django\clone_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
