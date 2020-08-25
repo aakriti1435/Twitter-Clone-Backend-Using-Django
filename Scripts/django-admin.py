cefd8ee2de7c308917fb8e1653dd18f18ce591bf
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2020-08-25 16:52:53 +0530

    removing extra files

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\aakri\OneDrive\Desktop\Twitter Clone\Dark Theme Twitter Clone Using Django\clone_env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
